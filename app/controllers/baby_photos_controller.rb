91a08c0432b05fe27cfc2a4b76f0b4d8b7b1411d
Kathleen Votolato <kvotolato3@gmail.com>
2015-03-01 21:29:44 -0500

    Initial steps to synthesize back-end
    - Relocate index.html from Public folder to appropriate view
    - Relocate photos from Public folder to assets/images folder
    - Transfer appropriate lines from orig. index.html to application layout
    - Add array containing baby photo urls to baby_photos controller
    - Modify index.html.erb such that it gets urls from controller variable
    - Added route pointed to baby_photos#index

--- app/controllers/baby_photos_controller.rb
+++ app/controllers/baby_photos_controller.rb
@@ -1,2 +1,18 @@
 class BabyPhotosController < ApplicationController
+  def index
+    @babyphotos = ['photos/Diane Blair 1.jpg',
+      'photos/Diane Blair 2.jpg',
+      'photos/Blair Hall 1.jpg',
+      'photos/Blair Hall 2.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg',
+      'photos/Kathleen Votolato.jpg']
+  end
 end
