41a81474ee441515f9f4ce7a859aef7470aefe8f
Kathleen Votolato <kvotolato3@gmail.com>
2015-03-13 00:40:46 -0400

    Add lock/unlock capability with a hard-coded password

--- app/assets/javascripts/flip.js
+++ app/assets/javascripts/flip.js
@@ -1,15 +1,36 @@
 $(document).ready(function(){
   $('.flip').click(App.flip);
+  $('.unlock-answers').click(App.unlockAnswers);
+  $('.lock-answers').click(App.lockAnswers);
 });
 
 var App = App || {};
 
+var locked = true;
+
 App.flip = function() {
-  $(this).find('.card').addClass('flipped').mouseleave(function(){
+  if (locked === false) {
+    $(this).find('.card').addClass('flipped').mouseleave(function(){
     $(this).removeClass('flipped');
     });
   return false;
+  };
 };
 
+App.unlockAnswers = function() {
+  var guess = $('#pw').val();
+  if (guess === 'jose') {
+    locked = false;
+    $('.lock-answers').toggle();
+    $('.unlock-answers').toggle();
+    $('#pw').toggle();
+  };
+};
 
+App.lockAnswers = function() {
+  locked = true;
+  $('.lock-answers').toggle();
+  $('.unlock-answers').toggle();
+  $('#pw').val('').toggle();
+};
 
