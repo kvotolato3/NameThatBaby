fc98812b97ed79bdc63cc7de19ba521a61959c21
Kathleen Votolato <kvotolato3@gmail.com>
2015-03-12 21:36:09 -0400

    Added baby photos

--- app/controllers/baby_photos_controller.rb
+++ app/controllers/baby_photos_controller.rb
@@ -9,13 +9,13 @@ class BabyPhotosController < ApplicationController
       {answer: 'Karen Kline', url: 'photos/Karen Kline.jpg'},
       {answer: 'Regina Costa', url: 'photos/Regina Costa.jpg'},
       {answer: 'Bonnie Alexander', url: 'photos/Bonnie Alexander.jpg'},
+      {answer: 'Kelly Votolato', url: 'photos/Kelly Votolato.jpg'},
+      {answer: 'Brenda Hartman', url: 'photos/Brenda Hartman.jpg'},
+      {answer: 'Lina Keo', url: 'photos/Lina Keo.jpg'},
+      {answer: 'Martha Pearson', url: 'photos/Martha Pearson.jpg'},
       {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'}]
+      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato Recent.jpg'},
+      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato Recent.jpg'},
+      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato Recent.jpg'}]
   end
 end
