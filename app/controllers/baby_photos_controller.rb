de53c38e49cc12df421bfe14918629cf920fb61b
Kathleen Votolato <kvotolato3@gmail.com>
2015-03-21 15:26:32 -0400

    Add new photo

--- app/controllers/baby_photos_controller.rb
+++ app/controllers/baby_photos_controller.rb
@@ -1,12 +1,14 @@
 class BabyPhotosController < ApplicationController
   def index
     @babyphotos = [
-      {answer: 'Diane Blair', url: 'photos/Diane Blair 2.jpg'},
+      {answer: 'Jie Chen', url: 'photos/Jie Chen.jpg'},
+      {answer: 'Diane Blair', url: 'photos/Diane Blair 1.jpg'},
       {answer: 'Regina Costa', url: 'photos/Regina Costa.jpg'},
       {answer: 'Eva Csizmadia', url: 'photos/Eva Csizmadia.jpg'},
       {answer: 'Karen Kline', url: 'photos/Karen Kline.jpg'},
       {answer: 'Bonnie Alexander', url: 'photos/Bonnie Alexander.jpg'},
       {answer: 'Kelly Votolato', url: 'photos/Kelly Votolato.jpg'},
+      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'},
       {answer: 'Rita Csizmadia', url: 'photos/Rita Csizmadia.jpg'},
       {answer: 'Brenda Hartman', url: 'photos/Brenda Hartman.jpg'},
       {answer: 'Blair Hall', url: 'photos/Blair Hall 2.jpg'},
@@ -16,7 +18,6 @@ class BabyPhotosController < ApplicationController
       {answer: 'Mira Nakhle', url: 'photos/Mira Nakhle.jpg'},
       {answer: 'Sally Koss', url: 'photos/Sally Koss.jpg'},
       {answer: 'Cyndi Koss', url: 'photos/Cyndi Koss.jpg'},
-      {answer: 'Leona Worsley', url: 'photos/Leona Worsley.jpg'},
-      {answer: 'Kathleen Votolato', url: 'photos/Kathleen Votolato.jpg'}]
+      {answer: 'Leona Worsley', url: 'photos/Leona Worsley.jpg'}]
   end
 end
