fc4629ce9016d582f0632e00f3a270d76876440c
Kathleen Votolato <kvotolato3@gmail.com>
2015-11-12 09:40:03 -0500

    Cleanup seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 BabyPhoto.create([{name: 'Jie Chen', aws_s3_bucket: 'kathleen-namethatbaby', aws_s3_key: 'photos/Jie+Chen.jpg'},
       {name: 'Diane Blair', aws_s3_bucket: 'kathleen-namethatbaby', aws_s3_key: 'photos/Diane+Blair+1.jpg'},
       {name: 'Regina Costa', aws_s3_bucket: 'kathleen-namethatbaby', aws_s3_key: 'photos/Regina+Costa.jpg'},
