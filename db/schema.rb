a74ec9336503aafdea661667e3a89cb9819f5324
Kathleen Votolato <kvotolato3@gmail.com>
2016-01-15 22:24:06 -0500

    Add email column to the Players table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116031308) do
+ActiveRecord::Schema.define(version: 20160116032253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160116031308) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "game_id"
+    t.string   "email"
   end
 
 end
