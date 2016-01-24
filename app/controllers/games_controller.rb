0cde250b0affe48c4212427e7b7469f519c0a320
Kathleen Votolato <kvotolato3@gmail.com>
2016-01-24 14:36:19 -0500

    Added link to Play game from the show game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,18 @@
 class GamesController < ApplicationController
+ before_action :set_game, only: [:show, :play]
+
   def show
-    @game = Game.find(params[:id])
+  end
+
+  def play
+    @players = @game.players
   end
 
 private
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
   def game_params
     params.require(:game).permit(:id, :honoree_name, :event_date, :require_password, :password)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'players#index'
-
   get 'games/:id' => 'games#show'
+  get 'play_games/:id' => 'games#play', as: :game_play
 end
