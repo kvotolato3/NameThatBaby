26fa812a9444c239ed601fff8c13f2d8b93fe784
Kathleen Votolato <kvotolato3@gmail.com>
2016-01-31 12:11:28 -0500

    Reordered routes such that devise related routes are lower priority

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'players#index'
   #other resources remaining :index, :create, :destroy, :new
   resources :games, only:[:show, :edit, :update] do
@@ -9,4 +8,6 @@ Rails.application.routes.draw do
   end
 
   resources :players, only:[:show, :edit, :update]
+
+  devise_for :users
 end
