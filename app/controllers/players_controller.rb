dc79d715fce18efe020d41874250c8fb90efb4e9
Kathleen Votolato <kvotolato3@gmail.com>
2016-02-24 13:19:04 -0500

    Added ability to add players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,25 @@
 class PlayersController < ApplicationController
 before_action :set_player, only: [:edit, :update]
+before_action :set_game, only: [:new, :create]
+
   def index
     @players = Player.all
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    @player.game_id = @game.id
+
+    if @player.save
+      redirect_to @game, notice: 'Player was successfully added.'
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -21,6 +39,10 @@ before_action :set_player, only: [:edit, :update]
 private
   def set_player
     @player = Player.find(params[:id])
+  end
+
+  def set_game
+    @game = Game.find(params[:game_id])
   end
 
   def player_params
