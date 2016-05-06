f1427860a5cc76962cbc6624f911b2c048993edd
Kathleen Votolato <kvotolato3@gmail.com>
2016-05-05 22:14:48 -0400

    Added ability to convert guests to regular users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,43 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters
 
+  def new
+    if @email = session[:email]
+      @name = User.find_by(email: @email).name
+    end
+    super
+  end
+
+  def create
+    if @email = session[:email]
+      # Change email on Guest so the new User passes uniqueness constraint
+      temp_email = "guest_#{Time.now.to_i}@example.com"
+      User.find_by(email: @email).update(email: temp_email)
+    end
+    super
+    logging_in(temp_email)
+  end
+
+  def logging_in(email)
+    guest = User.find_by(email: email)
+
+    # reassign uploads from guest to current user
+    Upload.where(user: guest).each do |upload|
+      upload.update(user: current_user)
+    end
+
+    # reassign players from guest to current user
+    Player.where(user: guest).each do |player|
+      player.update(user: current_user)
+    end
+
+    # delete guest user
+    guest.destroy
+
+    # delete session variable
+    session[:email] = nil
+  end
+
   protected
 
   def configure_permitted_parameters
